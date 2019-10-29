99a0b715443524511214da055fd93e21eb0d1ba1
odan <odan3240@gmail.com>
2019-10-29 22:15:51 +0900

    init express

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,17 @@
+import express from "express";
+
 export function add(x: number, y: number): number {
   return x + y;
 }
 
-console.log("poyo");
+async function bootstrap() {
+  const app = express();
+
+  app.get("/", (_, res) => {
+    res.send("poyo");
+  });
+
+  app.listen(3000, () => console.log("http://localhost:3000"));
+}
+
+bootstrap().catch(err => console.error(err));
