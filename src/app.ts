15b490c0ff344cf44794eb665eab9d0421aa6b9a
odan <odan3240@gmail.com>
2019-12-31 23:44:05 +0900

    add passport-local

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import morgan from "morgan";
 
 export function add(x: number, y: number): number {
   return x + y;
@@ -7,9 +10,31 @@ export function add(x: number, y: number): number {
 async function bootstrap() {
   const app = express();
 
+  app.use(express.json());
+  app.use(express.urlencoded());
+  app.use(morgan("combined"));
+
+  passport.use(
+    new LocalStrategy((username, password, done) => {
+      console.log(username, password);
+      return done(null, { username });
+    })
+  );
+
   app.get("/", (_, res) => {
     res.send("poyo");
   });
+  app.post(
+    "/login",
+    passport.authenticate("local", {
+      failureFlash: false,
+      session: false
+    }),
+    (_, res) => {
+      console.log("poyo");
+      res.send("ok");
+    }
+  );
 
   app.listen(3000, () => console.log("http://localhost:3000"));
 }
